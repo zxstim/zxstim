f9f48297232a6b3413fb655517116db14a858e22
ZxStim <zxstim@gmail.com>
2026-01-01 17:48:11 +0700

    feat: updated config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  assetPrefix: "/writing",
-
   // Rewrite rules for the writing domain
   async rewrites() {
     return [
