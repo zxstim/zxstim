450f48d02b40a5c3f2acd60a1089eabf3b19a53e
ZxStim <zxstim@gmail.com>
2026-01-01 17:42:26 +0700

    feat: updated multi zones

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: "/writing",
+
+  // Rewrite rules for the writing domain
+  async rewrites() {
+    return [
+      {
+        source: "/writing",
+        destination: `${process.env.WRITING_DOMAIN_URL}`,
+      },
+      {
+        source: '/writing/docs/:path+',
+        destination: `${process.env.WRITING_DOMAIN_URL}/docs/:path+`,
+      },
+      {
+        source: '/writing/blog/:path+',
+        destination: `${process.env.WRITING_DOMAIN_URL}/blog/:path+`,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
